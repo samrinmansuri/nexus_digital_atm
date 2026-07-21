0cf7f210ea9856c041e390b3cd7d48de70bf2f47
samrinmansuri <samrinmansuri6623@gmail.com>
2026-07-21 13:56:14 +0530

    initial project setup with function prototypes

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+
+// Function Prototypes
+void displayDateTime();
+int verifyPin(int pin);
+void showMenu();
+float withdraw(float balance);
+float deposit(float balance);
+void checkBalance(float balance);
