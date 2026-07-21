19e80f1a00bc3a905e9bedd25ff0e359d285a74d
samrinmansuri <samrinmansuri6623@gmail.com>
2026-07-21 15:57:10 +0530

    add date and time display

--- main.c
+++ main.c
@@ -10,3 +10,15 @@ void showMenu();
 float withdraw(float balance);
 float deposit(float balance);
 void checkBalance(float balance);
+
+int main()
+{
+    int pin = 9876;
+    int option;
+    int continue_transaction = 1;
+    float balance = 8765;
+
+    displayDateTime();
+
+    printf("\n\t\t\t**** WELCOME TO HDFC BANK ****\n");
+    
